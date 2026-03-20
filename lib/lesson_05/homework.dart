f4d69d34a17fb36fc6040b8ce43d9a79a54feb64
Yuliia Buhaiova <julia.bug0021@gmail.com>
2026-03-20 10:17:32 +0200

    loops (for)

--- lib/lesson_05/homework.dart
+++ lib/lesson_05/homework.dart
@@ -172,7 +172,21 @@ void task7() {
 /// Виведіть тільки парні числа.
 /// Обчисліть суму чисел від 1 до 10.
 
-void task8() {}
+void task8() {
+  for (var i = 1; i <= 10; i++) {
+    print('Число: $i');
+  }
+  for (var i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      print('Парне число: $i');
+    }
+  }
+  var counter = 0;
+  for (var i = 1; i <= 10; i++) {
+    counter = counter + i;
+  }
+  print('Сума: $counter');
+}
 
 /// Завдання 9: Цикл while
 /// Почніть із числа 10.
