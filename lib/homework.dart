8607a6c34f0aeddc7f3fda44d20d20a9705a7aa2
Yuliia Buhaiova <julia.bug0021@gmail.com>
2026-04-19 12:20:43 +0300

    feat: complete lesson 10 homework task 2

--- lib/homework.dart
+++ lib/homework.dart
@@ -9,4 +9,24 @@ void main() async {
 
   final result = await fetchName();
   print('Мене звати $result');
+
+  //task 2
+  Future<int> fetchAge() async {
+    await Future<void>.delayed(const Duration(milliseconds: 1500));
+    return 29;
+  }
+
+  final age = await fetchAge();
+  if (age % 100 >= 11 && age % 100 <= 14) {
+    print('Мені $age років');
+    return;
+  }
+  switch (age % 10) {
+    case 1:
+      print('Мені $age рік');
+    case 2 || 3 || 4:
+      print('Мені $age роки');
+    case 5 || 6 || 7 || 8 || 9 || 0:
+      print('Мені $age років');
+  }
 }
